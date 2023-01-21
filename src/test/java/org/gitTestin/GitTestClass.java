f55f3cf72195cd63fbe201c12b6447b8778ee9e0
pedururaj <pedurupeter3@gmail.com>
2023-01-22 01:41:02 +0530

    first time git testing

---
+++ src/test/java/org/gitTestin/GitTestClass.java
@@ -0,0 +1,20 @@
+package org.gitTestin;
+
+public class GitTestClass {
+	
+	public void class1() {
+		System.out.println("10.30am");
+
+	}
+	public void class2() {
+		System.out.println("00.01pm");
+
+	}
+	public void class3() {
+		System.out.println("00.04pm");
+
+	}
+	
+	
+
+}
